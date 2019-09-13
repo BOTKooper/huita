f536a0c745071d680abd4a6f4ab2d4be1397a2c7
BOTKooper <1337raphyhz228@gmail.com>
2019-09-13 12:08:59 +0300

    finished all tasks, excepting lab log

--- ProgrTech_Lab1/Task01/Program.cs
+++ ProgrTech_Lab1/Task01/Program.cs
@@ -5,16 +5,17 @@ namespace Task01
     {
         public static void Main(string[] args)
         {
-            Console.SetWindowSize(400, 400);
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetWindowSize(400, 170);
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.Title = "Custom title"; 
             Console.Beep();
             Console.WriteLine("Input your name:");
             string s = Console.ReadLine();
             Console.WriteLine(("Hello, {0}"), s);
             DateTime dt = DateTime.Now;
-            Console.WriteLine("Today os {0}, {1:D2}.{2:D2}.{3:D2}", dt.DayOfWeek, dt.Day, dt.Month, dt.Year);
+            Console.WriteLine("Today is {0}, {1:D2}.{2:D2}.{3:D2}", dt.DayOfWeek, dt.Day, dt.Month, dt.Year);
+            Console.WriteLine("Time: {0:D2}:{1:D2}:{2:D2}", dt.Hour, dt.Minute, dt.Second);
         }
     }
 }
